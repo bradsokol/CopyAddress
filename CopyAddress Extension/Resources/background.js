670545858109629d280eecaa756baed4a4143f1e
Brad Sokol <bsokol@gmail.com>
2025-11-11 13:49:28 -0500

    Copy tab address to the clipboard

--- CopyAddress Extension/Resources/background.js
+++ CopyAddress Extension/Resources/background.js
@@ -5,6 +5,27 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
         return Promise.resolve({ farewell: "goodbye" });
 });
 
-browser.action.onClicked.addListener((tab) => {
-    console.log("Toolbar button clicked");
+browser.action.onClicked.addListener(async (tab) => {
+    try {
+        const url = tab.url;
+        
+        // Inject a script into the active tab to copy URL to clipboard
+        // This is necessary because service workers don't have direct clipboard access
+        await browser.scripting.executeScript({
+            target: { tabId: tab.id },
+            func: async (urlToCopy) => {
+                try {
+                    await navigator.clipboard.writeText(urlToCopy);
+                    console.log("URL copied to clipboard:", urlToCopy);
+                } catch (err) {
+                    console.error("Failed to copy URL to clipboard:", err);
+                    throw err;
+                }
+            },
+            args: [url]
+        });
+    } catch (err) {
+        console.error("Failed to copy page URL:", err);
+        // Clipboard API might not be available on certain pages (e.g., chrome://, about:, etc.)
+    }
 });
