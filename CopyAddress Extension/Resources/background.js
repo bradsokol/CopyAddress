2aeb4e5b8737b444c40988a37da3637635c1124c
Brad Sokol <bsokol@gmail.com>
2025-11-16 11:33:51 -0500

    Clean up comments and unused code

--- CopyAddress Extension/Resources/background.js
+++ CopyAddress Extension/Resources/background.js
@@ -1,35 +1,26 @@
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
-
-    if (request.greeting === "hello")
-        return Promise.resolve({ farewell: "goodbye" });
-});
-
 browser.action.onClicked.addListener(async (tab) => {
-    try {
-        const url = tab.url;
-        const title = tab.title || url; // Use URL as fallback if title is empty/undefined
-        
-        // Format as Markdown link: [title](url)
-        const markdownLink = `[${title}](${url})`;
-        
-        // Inject a script into the active tab to copy Markdown link to clipboard
-        // This is necessary because service workers don't have direct clipboard access
-        await browser.scripting.executeScript({
-            target: { tabId: tab.id },
-            func: async (markdownLinkToCopy) => {
-                try {
-                    await navigator.clipboard.writeText(markdownLinkToCopy);
-                    console.log("Markdown link copied to clipboard:", markdownLinkToCopy);
-                } catch (err) {
-                    console.error("Failed to copy Markdown link to clipboard:", err);
-                    throw err;
-                }
-            },
-            args: [markdownLink]
-        });
-    } catch (err) {
-        console.error("Failed to copy page link:", err);
-        // Clipboard API might not be available on certain pages (e.g., chrome://, about:, etc.)
-    }
+  try {
+    const url = tab.url;
+    const title = tab.title || url;
+
+    const markdownLink = `[${title}](${url})`;
+
+    // Inject a script into the active tab to copy Markdown link to clipboard
+    // This is necessary because service workers don't have direct clipboard access
+    await browser.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: async (markdownLinkToCopy) => {
+        try {
+          await navigator.clipboard.writeText(markdownLinkToCopy);
+        } catch (err) {
+          console.error("Failed to copy Markdown link to clipboard:", err);
+          throw err;
+        }
+      },
+      args: [markdownLink],
+    });
+  } catch (err) {
+    // Clipboard API might not be available on certain pages (e.g., chrome://, about:, etc.)
+    console.error("Failed to copy page link:", err);
+  }
 });
